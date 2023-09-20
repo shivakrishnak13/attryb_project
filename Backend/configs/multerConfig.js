dcd1bec46f713c5624cf4da6742e26f292955819
shivakrishnak13 <kosarishivakrishna@gmail.com>
2023-09-20 13:13:03 +0530

    image uploader test 2

--- Backend/configs/multerConfig.js
+++ Backend/configs/multerConfig.js
@@ -1,10 +1,8 @@
 const multer = require('multer');
-const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const uploadDir = path.join(process.cwd(), 'src', 'Backend', 'uploads');
-    cb(null, uploadDir);
+    cb(null, '../uploads'); 
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname); 
