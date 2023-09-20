ff5ba54733731ec43ff8f9ea34986f629c199a01
shivakrishnak13 <kosarishivakrishna@gmail.com>
2023-09-20 12:50:37 +0530

    image uploader updated

--- Backend/configs/multerConfig.js
+++ Backend/configs/multerConfig.js
@@ -1,8 +1,9 @@
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads'); 
+    cb(null, path.join(__dirname, '..', 'uploads')); 
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname); 
